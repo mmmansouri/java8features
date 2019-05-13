1ea2fff6977aa9bef839c0fd4b4abac113b255ab
MANSOURI Mohamed Moez <mansourimedmoez@gmail.com>
2019-05-14 01:17:05 +0200

    add abstract class

---
+++ src/main/java/org/mmm/java8features/defaultstaticmethods/SomeAbstractClass.java
@@ -0,0 +1,12 @@
+package org.mmm.java8features.defaultstaticmethods;
+
+public abstract class SomeAbstractClass {
+
+    String variable;
+
+    public SomeAbstractClass(){
+        this.variable="value";
+    }
+    abstract void method();
+
+}
